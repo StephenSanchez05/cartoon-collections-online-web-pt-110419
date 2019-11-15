03f47e4a650b70d692b0dd613865398d7f81aee0
Stephen Sanchez <stephensanchez05@gmail.com>
2019-11-15 21:51:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ dwarves.each_with_index do |name, index|
 end
 
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(call)
+  call.collect {|element| element +"!"
 end
 
 def long_planeteer_calls# code an argument here
