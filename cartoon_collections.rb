11f1030ca80859333300864f71bccd235eb10e37
Stephen Sanchez <stephensanchez05@gmail.com>
2019-11-15 21:37:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,9 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
-end
+def roll_call_dwarves(dwarves)# code an argument here
+   i = 0
+  while i < roll_call_dwarves.length
+  block_return_values = [] << yield(collection[i])
+    i = i + 1 
+  end
 
 def summon_captain_planet# code an argument here
   # Your code here
