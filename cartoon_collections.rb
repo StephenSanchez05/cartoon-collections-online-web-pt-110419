c3d6ba1c236bd40a23347fb3413c97c6c89416b6
Stephen Sanchez <stephensanchez05@gmail.com>
2019-11-15 21:41:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,6 @@ def roll_call_dwarves(dwarves)# code an argument here
 dwarves.each_with_index |name, index|
   puts "#{name[index]}"
   end
-  end
 
 def summon_captain_planet# code an argument here
   # Your code here
