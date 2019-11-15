063a80c1d82f4c1be791342e3cdd9d4a7d89715b
Stephen Sanchez <stephensanchez05@gmail.com>
2019-11-15 22:16:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,6 @@ end
 
 def find_the_cheese(food)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  food.detect do 
-  food.include?(cheese_types)
+  food.detect do |potential|
+  cheese_types.include?(potential)
 end
